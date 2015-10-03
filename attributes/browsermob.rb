151e4b7f6a42bc42437a6053b5bf48c0c629dad6
Issa Ashwash <iashwash@bitsighttech.com>
2015-10-03 11:47:01 -0400

    License and documentation update

--- attributes/browsermob.rb
+++ attributes/browsermob.rb
@@ -1,7 +1,7 @@
 # Browsermob does not ship a prebuilt package.
 # In order to install browsermob, you need to follow instructions
 # at the source: https://github.com/lightbody/browsermob-proxy
-# Then hose this file somewhere and override the 'source' attribute:
+# Then host this file somewhere and override the 'source' attribute:
 default['harstorage']['browsermob']['source'] = ''
 default['harstorage']['browsermob']['install-directory'] = node['harstorage']['code-directory']
 # download & install deb
